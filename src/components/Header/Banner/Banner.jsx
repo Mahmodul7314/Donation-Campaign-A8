c0049555f0647ca3d14fa524a024047fc1501e7e
Mahmodul7314 <mahmudulhasan7314@gmail.com>
2023-09-27 10:41:17 -0700

    mobile and tablet full responsive complete

--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
         <div className="relative">
         <img style={{boxShadow:"#FFFFFFF2", }} className="opacity-10" src="https://i.ibb.co/2kYp1ZN/Rectangle-4281.png" alt="" />
         </div>
-        <div className="absolute  lg:-mt-72 lg:ml-30 ml-10 -mt-32 lg:ml-80  lg:w-3/4 mx-auto">
+        <div className="absolute  lg:-mt-72 md:ml-60 lg:ml-30 ml-10 md:-mt-56 -mt-32 lg:ml-80  lg:w-3/4 mx-auto">
             <h1 className="text-[#0B0B0B] text-2xl lg:text-5xl font-medium lg:font-extrabold pb-4 lg:pb-12  ">I Grow By Helping People In Need</h1>
             <div className="lg:ml-36 ml-14 lg:w-full">
             <input className=" w-36 lg:w-96 border-2  border-inherit h-8 lg:h-12 p-4 lg:p-6 px-2 "  type="text" alt="" placeholder="Search here...."/>
